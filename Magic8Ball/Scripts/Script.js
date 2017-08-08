362cdd8c894749d20fb1e12a7309adb19fdf715e
Your Name <you@example.com>
2017-08-08 15:17:17 -0400

    added in-class practice

---
+++ Magic8Ball/Scripts/Script.js
@@ -0,0 +1,10 @@
+﻿function getRandom() {
+    var num = Math.round(Math.random() * 5); //generate number between 0 and 65
+    return num;
+}
+
+var messages = ["Most likely", "Yes", "No", "Very doubtful", "Ask again later", "Better not tell you now"];
+
+function getMessage() {
+    document.getElementById("message").innerHTML = messages[getRandom()];
+}
